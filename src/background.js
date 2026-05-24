91b8594eafd6ad4e6a090974017e3985889ba5de
GLDMiningLabs <gld31.1gold@gmail.com>
2026-05-24 03:35:32 -0300

    Update background.js

--- src/background.js
+++ src/background.js
@@ -64,7 +64,7 @@ export const backgroundLinearGradient = (engine) => {
   if (checkMoveDown(engine)) {
     engine.setVariable(
       constant.bgLinearGradientOffset
-      , offsetHeight + (getMoveDownValue(engine) * 1.5)
+      , offsetHeight + (getMoveDownValue(engine) * 0.9)
     )
   }
   const colorIndex = parseInt(offsetHeight / engine.height, 10)
