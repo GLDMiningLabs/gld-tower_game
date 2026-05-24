d1e2d1ca833fbc35a46c6db96c555389cdb22416
GLDMiningLabs <gld31.1gold@gmail.com>
2026-05-24 03:54:08 -0300

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -61,7 +61,7 @@ export const getSwingBlockVelocity = (engine, time) => {
   if (engine.getVariable(constant.hardMode)) {
     hard = 1.1
   }
-  return Math.sin(time / (200 / hard))
+  return Math.sin(time / (600 / hard))
 }
 
 export const getLandBlockVelocity = (engine, time) => {
